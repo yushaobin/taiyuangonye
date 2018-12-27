eeda0231d58ec6834caf75ab6bf21bed8eab4d63
152056225 <2075773592@qq.com>
2018-12-27 18:10:32 +0800

    home work

---
+++ 李春慧/oncourse/pages/all_content/index.js
@@ -0,0 +1,97 @@
+// pages/all_content/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    items: [
+      {
+        "date": "12.26",
+        "title": "种族主义者甘地:一个不为人知的真相",
+        "img"  : ""
+        },
+      {
+        "date": "12.25",
+        "title": "比尔.盖茨2018年书单分享",
+        "img"  : ""  
+    },
+    {
+        "date": "12.24",
+        "title": "经典的圣诞歌都在这里了",
+        "img"  : "/img/check.png"
+      },
+      {
+        "date": "12.23",
+        "title": "《卫报》:我们正面临者第六次大灭绝",
+        "img"  : "/img/check.png"
+      },
+      {
+        "date": "12.22",
+        "title": "《蜘蛛侠:平行宇宙》体验报告:这是你从没有看过的全新版本",
+        "img"  : "/img/check.png"
+      },
+      {
+        "date": "12.21",
+        "title": "超半数95后想从事的职业,真的赚钱吗?",
+        "img"  : "/img/check.png"
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
